c7f47146b4adb7231311d038943568784776a658
Newtype1994 <aaatibet@hotmail.com>
2019-01-26 01:38:09 +0700

    Add enemy controller

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private int lifetime = 1500;
+    private int healthPoint = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().AddTorque(new Vector3(0,0,60));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= 1;
+        if (lifetime == 0) enemyDie();
+    }
+
+    private void enemyDie()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void hurt()
+    {
+        healthPoint -= 1;
+        if (healthPoint == 0) enemyDie();
+    }
+}
