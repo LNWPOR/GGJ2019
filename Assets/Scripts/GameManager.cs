f3372f42d4dd1a5d73ccc86ca0fad8b90e1cf2de
ckhoarmnp <sorapol57@gmail.com>
2019-01-26 01:32:55 +0700

    init script folder and add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  private static GameManager _Instance;
+  public static GameManager GetInstance()
+  {
+    if (_Instance == null) _Instance = new GameManager();
+    return _Instance;
+  }
+  private STATE _GameState;
+
+  public enum STATE
+  {
+    GamePlay,
+    BossFight
+  }
+
+  public void DoPlayerDead()
+  {
+    Debug.Log("<color=red>DoPlayerDead.</color>");
+    SceneManager.LoadScene("SampleScene");
+  }
+
+  public void DoBossDead()
+  {
+    Debug.Log("<color=green>DoBossDead.</color>");
+    SceneManager.LoadScene("SampleScene");
+  }
+
+  public void DoBossFight()
+  {
+    _GameState = STATE.BossFight;
+    Debug.Log("<color=orange>Now game is state : </color>" + _GameState);
+  }
+
+  protected void Awake()
+  {
+    DoPlayerDead();
+    DoBossDead();
+    DoBossFight();
+  }
+}
