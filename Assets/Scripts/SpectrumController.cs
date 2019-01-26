415bbaa1df62c4e29fdc6f6385ced3abfe06beb6
ckhoarmnp <sorapol57@gmail.com>
2019-01-26 16:37:34 +0700

    make scene for test feeling and make init transition.

--- Assets/Scripts/SpectrumController.cs
+++ Assets/Scripts/SpectrumController.cs
@@ -149,16 +149,11 @@ public class SpectrumController : MonoBehaviour
         {
             Vector3 previousScale = cubes[i].transform.localScale;
             Vector3 tempPos = orgPos[i];
-            //previousScale.x = Mathf.MoveTowards (previousScale.x, spectrum [i] * 200, spectrum [i] * 200);
+            if (spt[i] > 0.02f) spt[i] = 0.02f;
             previousScale.x = Mathf.Lerp(previousScale.x, spt[i] * 1000, Time.deltaTime * 30);
             tempPos = orgPos[i] + cubes[i].transform.right * (previousScale.x / 2.0f + orgScale.x / 2.0f);
             cubes[i].transform.localScale = previousScale;
             cubes[i].transform.position = tempPos;
         }
-    }
-
-    void CreateEnemyByChunk()
-    {
-
     }
 }
