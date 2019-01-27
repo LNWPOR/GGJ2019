83e7257f944a2412f955190581c701d8d1df45bd
ckhoarmnp <sorapol57@gmail.com>
2019-01-27 10:32:21 +0700

    add coolest transition ever

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class SceneController : MonoBehaviour
 {
     private readonly float MAX_TEXT_SIZE = 1.5f;
-    private const float SCALE_UP_TIME = 1f;
+    private const float SCALE_UP_TIME = 2f;
     private const float FADE_TIME = 1f;
     private const float ZOOM_TIME = 1f;
 
@@ -21,17 +21,54 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _Camera.GetComponent<Camera>().enabled = true;
         _Canvas.SetActive(true);
         StartCoroutine(DoScaleUpText(() =>
         {
             StartCoroutine(DoFade(()=> 
             {}));
-            StartCoroutine(DoZoom(() => { Destroy(_Canvas); }));
+            StartCoroutine(DoZoom(() => { _Canvas.SetActive(false); }));
         }));
+    }
+
+    public void DoPlayerDead()
+    {
+        StartCoroutine(DoFadeOut(()=> { GameManager.GetInstance().DoRestartScene(); }));
+    }
+
+    IEnumerator DoFadeOut(System.Action callback)
+    {
+        _Canvas.SetActive(true);
+        float time = 0f;
+        while (time < FADE_TIME)
+        {
+            float frac = time / FADE_TIME;
+            _FadeImage.color = new Color(_FadeImage.color.r, _FadeImage.color.g, _FadeImage.color.b, frac);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        _FadeImage.color = new Color(_FadeImage.color.r, _FadeImage.color.g, _FadeImage.color.b, 1f);
+        callback();
+        _Camera.GetComponent<Camera>().enabled = false;
     }
 
     IEnumerator DoScaleUpText(System.Action callback)
     {
+        if (GameManager.GetInstance().GetDeadCount() > 0)
+        {
+            if (GameManager.GetInstance().GetDeadCount() < 2)
+            {
+                _GreetText.text = "Welcome home again.";
+            }
+            else if (GameManager.GetInstance().GetDeadCount() < 5)
+            {
+                _GreetText.text = "Look like you love this home.";
+            }
+            else
+            {
+                _GreetText.text = "Hmmm.. try harder pls. noob.";
+            }
+        }
         float time = 0f;
         while (time < SCALE_UP_TIME)
         {
